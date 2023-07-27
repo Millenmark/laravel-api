c28c81d20337d098e5eb0fd3b1c200d5f7f9afd2
Millen Mark Aquino <millenmark.iv@gmail.com>
2023-07-28 07:13:56 +0800

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -3,17 +3,18 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+
+// Customers
+Route::apiResource('/customers', 'CustomerController')
+    ->only(['index', 'show', 'store', 'update', 'destroy']);
+
+// Invoices
+Route::apiResource('/invoices', 'InvoiceController')
+    ->only(['index', 'show', 'store', 'destroy']);
+
+Route::post('invoices/bulk', 'InvoiceController@bulkStore')->name('invoice.bulk');
